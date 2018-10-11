bbfae911168dc2c5a54f7edb36ad2c8e3376ae53
kapoorlab <randomaccessiblekapoor@gmail.com>
2018-10-11 18:21:14 +0200

    256, 128 , UNet comparision with limited training data

--- GenerateConnectedComps.py
+++ GenerateConnectedComps.py
@@ -1,15 +1,30 @@
+import sys
+sys.path.insert(0, "/data/u934/service_imagerie/v_kapoor/anaconda2/bin/PyImage/utils")
+from scipy import ndimage
+from stardist import fill_label_holes
+import scipy
+import numpy as np
+from skimage import measure
+from skimage import filters
+from skimage.segmentation import find_boundaries
+from skimage.morphology import remove_small_objects, binary_erosion
+from skimage.filters import threshold_otsu, threshold_mean
+from skimage.exposure import rescale_intensity
+from Normalize import Path,normalizeMinMax
 
 def ConnecProbability(img, minsize):
       
-      binary  =  img > 0
-     
+      thresh = threshold_mean(img)
+      binary  =  img > thresh
+      binary = binary_erosion(binary)  
+      binary = fill_label_holes(binary[:,:] )
       labels, nr_objects = ndimage.label(binary > 0) 
       nonormimg = fill_label_holes(labels[:,:] )
       nonormimg = remove_small_objects(nonormimg, min_size=minsize, connectivity=4, in_place=False)
       min = np.amin(nonormimg)
       max = np.amax(nonormimg)
       nonormimg = normalizeMinMax(nonormimg, min, max) 
-        
+      
       labels = nonormimg
       return labels
     
